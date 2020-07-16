175466fceecd80966ea76da807f21347fae69bf4
Evgenii Goriunov <belerafont@gmail.com>
2020-07-16 08:47:56 +0100

    Actual first commit. Add main script with some draft

---
+++ torrents_cleanup.py
@@ -0,0 +1,84 @@
+import os
+import re
+import time
+
+'''
+Useful links:
+torrent file structure: https://fileformats.fandom.com/wiki/Torrent_file
+open file with buffer for byte-read: http://www.djangospin.com/python-file-buffering/
+RegExp for byte search: https://stackoverflow.com/questions/31019854/typeerror-cant-use-a-string-pattern-on-a-bytes-like-object-in-re-findall
+'''
+
+def main():
+    startTimer = time.perf_counter()
+
+
+    fileTest = "C:/Users/test/Downloads/Форсаж The Fast and the Furious (Роб Коэн Rob Cohen) [2001, США, боевик, триллер, криминал, WEB-DLRip-AVC] [Open Matte] Dub + [rutracker-5433397].torrent"
+
+
+    # Scan 'Downloads' folder for torrent files.
+    # Store them in lstTorrentFiles[(path, file)]
+    lstTorrentFiles = []
+    reTorrentFile = re.compile('.+\.torrent')
+    for root, dirs, files in os.walk('C:/Users/test/Downloads'):
+        for file in files:
+            if reTorrentFile.match(file):
+                lstTorrentFiles.append((root, file))
+
+
+    for file in lstTorrentFiles:
+        filePath, fileName = file
+
+        # open file with buffer for byte search
+        with open(f"{filePath}\{fileName}", buffering=5) as readFile:
+            rawFileContent = readFile.buffer
+            torrentHeader = rawFileContent.readline()
+
+            # RegExp should be with rb"mask" for byte search
+            reInfoD = re.search(rb"infod(\d+):", torrentHeader)
+
+            currentPos = reInfoD.span()[1]
+            reNameD = re.search(rb"4:name(\d+):", torrentHeader[currentPos::])
+            nameLength = int(reNameD.group(1))
+
+            currentPos += reNameD.span()[1]
+            print(f"file:\t\t\t{fileName}\nname {nameLength}:\t\t{torrentHeader[currentPos:currentPos+nameLength].decode('UTF-8')}\n\n")
+
+    # # Single file torrent - infod6:length (==6)
+    # # Multi files torrent - infod5:files (==5)
+    # boolSingleFile = reInfoD.group(1) == "6"
+    # print(boolSingleFile)
+    #
+    # currentPos = reInfoD.span()[1]
+    # # if it's a single file torrent
+    # if boolSingleFile:
+    #     reNameD = re.search(r"4:name(\d+):", torrentHeader[currentPos::])
+    #     nameLength = int(reNameD.group(1))
+    #     currentPos += reNameD.span()[1]
+    #     print(torrentHeader[currentPos:currentPos+nameLength])
+    # # if it's a multi files torrent
+    # else:
+    #     reNameD = re.search(r"4:name(\d+):", torrentHeader[currentPos::])
+    #     nameLength = int(reNameD.group(1))
+    #     currentPos += reNameD.span()[1]
+    #     print(torrentHeader[currentPos:currentPos+nameLength])
+
+
+    # reBencodingSting = re.compile('(\d+):')
+    # for i in range(0, len(torrentHeader)):
+    #     char = torrentHeader[i]
+    #     if char == 'd':
+    #         dictTorrent = {}
+    #         if reBencodingSting.match(torrentHeader[i+1::]): keyMatch = int(reBencodingSting.match(torrentHeader[i+1::]).group()[0])
+    #         print(keyMatch)
+
+
+
+
+    finishTimer = time.perf_counter()
+    print(f"Finished in {round(finishTimer-startTimer, 2)} second(s)")
+
+
+
+if __name__ == '__main__':
+    main()
